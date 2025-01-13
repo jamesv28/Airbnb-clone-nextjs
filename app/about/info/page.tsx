cd8e200f0c844b721fb4632b6b4c8ddad53c74e7
James Volmert <jamesvolmert@Jamess-iMac.local>
2025-01-13 16:43:24 -0700

    setup navbar

---
+++ app/about/info/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const InfoPage = () => {
+  return (
+    <div>
+      <h1>Info page</h1>
+      <Link href="/">Back Home</Link>
+    </div>
+  );
+};
+
+export default InfoPage;
